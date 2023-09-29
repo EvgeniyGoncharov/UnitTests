a1a2b9c27bb0c5453b3a3e07a728db0b3bb8b6aa
Evgen <Evgen@DESKTOP-TFNLO9V>
2023-09-29 21:50:53 +0300

    Дописал юнит тесты для моделей

--- untitled/src/ru/miet/testing/ModelTest.java
+++ untitled/src/ru/miet/testing/ModelTest.java
@@ -4,19 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ModelTest {
 
+    Model model;
     @org.junit.jupiter.api.Test
     void sum() {
+        double a = 1;
+        assertEquals(a + a, model.sum(a,a));
     }
 
     @org.junit.jupiter.api.Test
     void subtract() {
+        double a = 1;
+        assertEquals(a - a, model.subtract(a,a));
     }
 
     @org.junit.jupiter.api.Test
     void multiply() {
+        double a = 1;
+        assertEquals(a * a, model.multiply(a,a));
     }
 
     @org.junit.jupiter.api.Test
-    void divide() {
+    void divide() throws Exception {
+        double a = 1;
+        assertEquals(a / a, model.divide(a,a));
     }
 }
